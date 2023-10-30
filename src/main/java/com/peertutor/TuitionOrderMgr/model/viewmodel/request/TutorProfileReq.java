090db9ad1fe98c51bfec5893afdb954003db331c
Liu Yiwen <39231750+0blivious@users.noreply.github.com>
2023-10-30 17:37:56 +0800

    Update TutorProfileReq.java

--- src/main/java/com/peertutor/TuitionOrderMgr/model/viewmodel/request/TutorProfileReq.java
+++ src/main/java/com/peertutor/TuitionOrderMgr/model/viewmodel/request/TutorProfileReq.java
@@ -6,12 +6,12 @@ public class TutorProfileReq {
 
     public String name;
 
-    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Only alphanumeric characters and underscore are allowed")
+    @Pattern(regexp = "^[a-zA-Z0-9_ ]{1,20}$", message = "Only alphanumeric characters and underscore are allowed")
     public String displayName;
 
     public String accountName;
 
-    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Only alphanumeric characters and underscore are allowed")
+    @Pattern(regexp = "^[a-zA-Z0-9_ ]{0,255}$", message = "Only alphanumeric characters and underscore are allowed")
     public String introduction;
 
     public String subjects;
